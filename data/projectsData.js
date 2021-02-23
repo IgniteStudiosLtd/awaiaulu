0c6f7ec0213497e7d9f1361c47dde8931070f5f6
Kingi Gilbert <kingi@ignitestudios.org>
2021-02-23 18:50:13 +1300

    Tests

--- data/projectsData.js
+++ data/projectsData.js
@@ -1,7 +1,7 @@
 const projectsData = [
   {
-    title: 'A Search Engine',
-    description: `What is you could look up any information in the world? Webpages, images, videos
+    title: 'A Kaupapa',
+    description: `Yozah. What is you could look up any information in the world? Webpages, images, videos
     and more. Google has many features to help you find exactly what you're looking
     for.`,
     imgSrc: '/static/images/google.png',
@@ -14,6 +14,14 @@ const projectsData = [
     forgotten handphones with this simple yet affordable solution.`,
     imgSrc: '/static/images/time-machine.jpg',
     href: '/blog/the-time-machine',
+  },
+  {
+    title: 'Tests 3',
+    description: `Imagine being able to travel back in time or to the future. Simple turn the knob
+    to the desired date and press "Go". No more worrying about lost keys or
+    forgotten handphones with this simple yet affordable solution.`,
+    imgSrc: '/static/images/time-machine.jpg',
+    href: '/blog/tests-3',
   },
 ]
 
